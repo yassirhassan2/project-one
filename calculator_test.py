0ca6175f338e2261a2a3956479b1ccc73b372301
yasser hassan <yassirhassanh956@gmail.com>
2022-03-16 01:48:41 -0700

    just added 2 files

---
+++ calculator_test.py
@@ -0,0 +1,19 @@
+import unittest
+
+# This is the class we want to test. So, we need to import it
+import Calculator as CalculatorClass
+
+class Test(unittest.TestCase):
+    calculator = CalculatorClass.Calculator() # instantiate the Calculator Class
+    def test_0_add(self):
+        result = self.calculator.add(4,8)
+        self.assertEqual(result,12)
+    def test_0_substract(self):
+        result = self.calculator.subtract(10,2)
+        self.assertEqual(result,8)
+    def test_0_multi(self):
+        result = self.calculator.multi(8,2)
+        self.assertEqual(result,16)
+if __name__ == '__main__':
+    # begin the unittest.main()
+    unittest.main()
